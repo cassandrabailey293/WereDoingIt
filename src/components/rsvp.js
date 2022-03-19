6399495650e5e65f32dcae61243bea7129a43811
cassandra.bailey293 <cassandra.bailey293@gmail.com>
2022-03-18 22:49:53 -0400

    added firebase to app

--- src/components/rsvp.js
+++ src/components/rsvp.js
@@ -1,3 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBy0hcB_NxD7vCDFslxDDq_19b8wNEsxCQ",
+    authDomain: "wedding-site-7d128.firebaseapp.com",
+    projectId: "wedding-site-7d128",
+    storageBucket: "wedding-site-7d128.appspot.com",
+    messagingSenderId: "446321917636",
+    appId: "1:446321917636:web:d01f09b1f711365ff91391",
+    measurementId: "G-KFXW5NV2HK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+var db = getFirestore(app);
+
+
+addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+}).then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+}).catch((e) => {
+console.error("Error adding document: ", e);
+})
+
+
+
+
 function RSVP() {
   return <h1>RSVP</h1>;
 }
