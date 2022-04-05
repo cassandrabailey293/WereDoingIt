3dae9afccbe84a05c403aa820a59ef328fbac4c5
cassandra.bailey293 <cassandra.bailey293@gmail.com>
2022-04-05 19:32:13 -0400

    i made u some content

--- src/components/our-story.js
+++ src/components/our-story.js
@@ -7,7 +7,9 @@ function OurStory() {
         <h1 className="our-story-header">Our Story</h1>
           </ScrollableAnchor>
           <div className="our-story-details-wrapper">
-              
+              <div>Colorado was a new adventure for Cassy - when she moved from Virginia out West, she had no idea what to expect. Andrew, a “Colorado Native” (born & raised in St. Louis, MO), worked for a company called Connexta - the connection that drew the two together. The chemistry between them only continued to React - and with COVID ravaging the world, Andrew and Cass found light and love within each others’ company (after all, we couldn’t go anywhere or do much at the beginning!). Needless to say, they fell deeply in love and bought a house together in April of 2021. They now live there with their two cats, Otto and Piper (Rest in Peace Ezio, 2/4/22). They invite & welcome you in celebration of their union and their happily ever after (heart unicode) </div>
+              {/*put the pic here*/}
+
           </div>
     </div>
   );
@@ -16,6 +18,7 @@ function OurStory() {
 //class OurStoryDetails extends React.Component {
 //    render() {
 //        return <h1>OUR STORY DEETS</h1>;
+// this is so that we can use responsive react for different screen sizes..potentially
 //    }
 //}
 
