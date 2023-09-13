9ffdc66cfdb24bf273d142c21c3df6b40a3a3839
cassandrabailey293 <cassandra.bailey@virtru.com>
2023-09-13 11:43:32 -0400

    whoops

--- src/components/rsvp.js
+++ src/components/rsvp.js
@@ -28,15 +28,6 @@ import styled from "styled-components";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBy0hcB_NxD7vCDFslxDDq_19b8wNEsxCQ",
-  authDomain: "wedding-site-7d128.firebaseapp.com",
-  projectId: "wedding-site-7d128",
-  storageBucket: "wedding-site-7d128.appspot.com",
-  messagingSenderId: "446321917636",
-  appId: "1:446321917636:web:d01f09b1f711365ff91391",
-  measurementId: "G-KFXW5NV2HK",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
